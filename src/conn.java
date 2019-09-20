3312fc5dc4ab2c7859f5c91866b85345463859c5
laudit <>
2019-09-20 05:11:42 -0400

    added "add product"

---
+++ src/conn.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Acer Aspire E 15
+ */
+public class conn {
+    
+    
+public String driver = "com.mysql.jdbc.Driver";
+public String local = "jdbc:mysql://localhost/bsitact23?";
+public String sr = "";
+public String ps = "root";
+
+
+
+}
+
